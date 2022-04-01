5023ca1e8885dde08b965c11b800959abc150d84
unknown <kaithwas.697@gmail.com>
2022-04-01 11:51:16 +0530

    Ability to analyse the mood from users input

---
+++ MoodAnalyser/MoodAnalyser/MoodAnalysers.cs
@@ -0,0 +1,17 @@
+﻿namespace MoodAnalyserSpace
+{
+    public class MoodAnalysers
+    {
+        public MoodAnalysers()
+        {
+
+        }
+
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "SAD";
+            else return "HAPPY";
+        }
+    }
+}
